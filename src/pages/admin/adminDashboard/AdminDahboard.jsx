16cedb63bca718d08d0e152c169578f949286632
Rushmitkarki <220046@softwarica.edu.np>
2024-06-15 20:59:45 +0545

    changes

--- src/pages/admin/adminDashboard/AdminDahboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./AdminDashboard.css";
-export default function AdminDashboard() {
-  return (
-    <h1></h1>
-    
-  
-  );
-}
---
+++ src/pages/admin/adminDashboard/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <>
+    
+
+    </>
+  )
+}
+
+export default AdminDashboard
+
