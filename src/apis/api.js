a812d6673d164613e301591f680c56f70d40625f
Rushmitkarki <220046@softwarica.edu.np>
2024-07-09 22:59:02 +0545

    finish

--- src/apis/api.js
+++ src/apis/api.js
@@ -65,6 +65,8 @@ export const getSingleUser = () => Api.get(`/api/user/get_single_user`, config);
 
 // Fetch all users
 export const getAllUsers = () => Api.get("/api/user/get_all_users");
+// delete user
+export const deleteUser = (id) => Api.delete(`/api/user/delete_user/${id}`,config);
 
 // Update user profile
 export const updateUserProfile = (id, userData) =>
@@ -75,8 +77,11 @@ export const addToFavoriteApi = (data) =>
   Api.post("/api/favourite/add", data, config);
 
 // appointment doctors
-export const appointmentDoctor = (data) => Api.post("/api/booking/create_appointments",data)
+export const appointmentDoctor = (data) =>
+  Api.post("/api/booking/create_appointments", data);
 // fetch all user data
-export const getUsersWithAppointments = () => Api.get("/api/booking/users_with_appointments");
+export const getUsersWithAppointments = () =>
+  Api.get("/api/booking/users_with_appointments");
 // delete appointment
-export const deleteAppointment = (id) => Api.delete(`/api/booking/delete_appointments/${id}`,config);
+export const deleteAppointment = (id) =>
+  Api.delete(`/api/booking/delete_appointments/${id}`, config);
